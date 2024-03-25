469aa6516ee92716598b419cb544a2ec6d2d8202
Luiz Otávio da Rosa de Oliveira <luizotaviodarosadeoliveira@gmail.com>
2024-03-24 21:38:55 -0300

    Adição de arquivos
    Adição do arquivo Definições.py e Funções.py

---
+++ Definições.py
@@ -0,0 +1,34 @@
+from random import randint
+from Funções import JogarDado
+class Personagem:
+   def __init__(self,raca, cor, tamanho): 
+      self.cor = cor
+      self.ListaCor = ["AMARELO", "VERMELHO", "AZUL", "VERDE"]
+      self.tamanho = tamanho
+      self.raca = raca
+      self.ListaRaca = ["HUMANO", "ORC", "GIGANTE", "DRAGONETE", "FADA"]
+   def Mudarcor(self, nova_cor):
+      if nova_cor in self.ListaCor:
+         self.cor = nova_cor
+      else:
+         print("Cor invalida!!")
+      
+   def MudarTamanho(self, novo_tamanho):
+      if novo_tamanho >30 and novo_tamanho <200:
+         self.tamanho = novo_tamanho
+      else:
+         print("Tamanho invalido! ")
+      
+   def MudarRaca(self, nova_raca):
+      if nova_raca in self.ListaRaca:
+         self.raca = nova_raca
+      else:
+         print("Raça invalida!")
+   
+   def MostrarInfo(self):
+        print(self.cor)
+        print(self.tamanho,"cm")
+        print(self.raca)
+       
+
+cliente = Personagem("HUMANO","AMARELO", 130)
---
+++ Funções.py
@@ -0,0 +1,22 @@
+from random import randint
+def PerguntaNome():
+   str(input("Digite seu nome: "))
+   
+   
+def ImparPar():
+   num = int(input("Digite um valor: "))
+   if num%2 == 0:
+      print("Par")
+   else:
+      print("Impar")
+
+
+def ParImpar():
+   num = int(input("Digite um valor: "))
+   if num%2 == 0:
+      return 1
+   else:
+      return 0
+
+def JogarDado():
+   print(randint(1, 20))
