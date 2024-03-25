dc40b0f1cdbc1b0677310cdda5feaf21418a50b8
Luiz Otávio da Rosa de Oliveira <luizotaviodarosadeoliveira@gmail.com>
2024-03-24 21:32:57 -0300

    Create jogo.py

---
+++ jogo.py
@@ -0,0 +1,14 @@
+from Definições import *
+from Funções import JogarDado
+while True:  
+    dado = randint(1, 20)
+    cliente.Mudarcor(str(input("Digite sua cor: ")).upper().strip())
+    cliente.MudarTamanho(int(input("Digite seu tamanho: ")))
+    cliente.MudarRaca(str(input("Digite sua raça: ")).upper().strip())
+    cliente.MostrarInfo()
+    escolha = str(input("Quer jogar um dado?")).upper()[0]
+    if escolha == "S":
+        JogarDado()
+    else:
+        print("Tudo bem! ")
+   
